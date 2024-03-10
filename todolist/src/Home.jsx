e20a0e1d93e72bd4a5b134290c8b043430445467
nithyarg <155550098+nithyarg@users.noreply.github.com>
2024-03-10 08:59:10 +0000

    7m complted

---
+++ todolist/src/Home.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react'
+
+function Home() {
+  const [todos,settodos] =useState([])
+  return (
+    <div>
+      <h2>Todo List</h2>
+      <Create/>
+      {
+        todos.length === 0 
+        ?
+        <div><h2>No Record</h2></div>
+        :
+        todos.map(todo =>(
+          <div>
+            {todo}
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default Home
