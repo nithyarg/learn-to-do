f7833106bfc4070cfd56fc04cfb5f40c1b0daccf
nithyarg <nithyaasbsc@gmail.com>
2024-03-10 20:46:51 +0530

    7.46m complted

---
+++ todolist/src/Create.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Create() {
+  return (
+    <div className='create_form'> 
+        <input type="text" name="" id="" placeholder='Enter Taskg'/>
+        <button type="button">Add</button>
+    </div>
+  )
+}
+
+export default Create
