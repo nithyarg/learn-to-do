77a3ac04583e65c9cdddfbf5786f8dc52c9004e1
nithyarg <nithyaasbsc@gmail.com>
2024-03-12 16:31:21 +0530

    23.15m

--- todolist/src/Home.jsx
+++ todolist/src/Home.jsx
@@ -1,20 +1,27 @@
- import React ,{ useState }from 'react'
+ import React ,{ useEffect, useState }from 'react'
 import Create from './Create'
+import axios from 'axios'
  
  function Home() {
     const [todos, setTodos] = useState([])
+    useEffect(() =>{
+      axios.get('http://localhost:3001/get')
+      .then(result => setTodos(result.data))
+      .catch(err => console.log(err))
+    },[])
    return (
      <div className='home'>
         <h2>Todo List</h2>
         <Create/>
+        <br />
         {
             todos.length === 0 
             ?
             <div><h2>No Record</h2></div> 
             :
             todos.map(todo =>(
-                <div>
-                    {todo}
+                <div className='task'>
+                    <p>{todo.task}</p>
                 </div>
             ))
         }
